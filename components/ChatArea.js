b53738cca70ea9ade10d10947e08884889e7cc89
Crypticwave <abhinavdatta38@gmail.com>
2021-03-20 11:51:31 +0530

    Setup ChatArea and state for input

---
+++ components/ChatArea.js
@@ -0,0 +1,15 @@
+import ChatNav from './ChatNav';
+import Chats from './Chats';
+import Input from './Input';
+
+export default function ChatArea(){
+  return(
+    <>
+      <ChatNav text="Chat"/>
+            <Chats />
+            <div className="object-none object-bottom">
+            <Input />
+            </div>
+    </>
+  );
+}
